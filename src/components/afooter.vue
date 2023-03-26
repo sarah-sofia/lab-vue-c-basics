9365671542f86d4cea7168885c9309cbedc9eee4
sarah-sofia <sarah.s3ch@gmail.com>
2023-03-26 19:18:26 +0200

    iteration 1 and 2

---
+++ src/components/afooter.vue
@@ -0,0 +1,22 @@
+<template>
+<h1>I'm a footer</h1>
+</template>
+
+<script>
+export default {
+    name: 'afooter',
+    data() {
+    return{
+        
+    }
+}
+}
+
+
+</script>
+
+<style scoped>
+h1 {
+    background-color: aqua;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="navbarPlatground">
+<h1>I'm a navbar</h1>
+<p>For the second iteration 2 + 2 is {{ 2 + 2 }}</p>
+<p>{{ getString() }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+    data() {
+        return {
+            name: 'Sarah',
+            age: 30
+        } 
+
+    },
+methods: {
+    getString() {
+        return `I used a getString method to write this: My name is ${this.name} and my age is ${this.age}`
+        }
+    }
+}
+        
+
+
+
+</script>
+
+<style scoped>
+.navbarPlatground {
+    background-color: darkgrey;
+    color: white;
+}
+</style>
